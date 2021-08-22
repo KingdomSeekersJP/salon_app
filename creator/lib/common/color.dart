5dc770bdd629cce9c1c289e27e345ee9cc777d17
mazemaze <yujiro2345@gmail.com>
2021-08-23 02:07:44 +0900

    Added an apply flow for  online salon

---
+++ creator/lib/common/color.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const Map<int, Color> color = {
+  50: Color(0xFFE4E4E7),
+  100: Color(0xFFBABDC2),
+  200: Color(0xFF8D919A),
+  300: Color(0xFF5F6471),
+  400: Color(0xFF3C4352),
+  500: Color(0xFF1A2234),
+  600: Color(0xFF171E2F),
+  700: Color(0xFF131927),
+  800: Color(0xFF0F1421),
+  900: Color(0xFF080C15),
+};
+
+const MaterialColor primaryColor = MaterialColor(0xFF1A2234, color);
