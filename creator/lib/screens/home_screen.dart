c7b92c6848ac2e125deeff5db96e9ea2193b567a
Hiroki Kondo <kondo.matr02@gmail.com>
2021-09-23 21:53:40 +0900

    いつでもログアウト出来るように実装

--- creator/lib/screens/home_screen.dart
+++ creator/lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:creator/screens/login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,6 +11,38 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.green,
         title: Text('ホーム'),
+        iconTheme: IconThemeData(color: Colors.black),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: Text(
+                "testPage",
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white,
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text("ログアウト"),
+              onTap: () async {
+                await FirebaseAuth.instance.signOut();
+                await Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return LoginScreen();
+                    },
+                  ),
+                );
+              },
+            )
+          ],
+        ),
       ),
       body: Center(
         child: Text(
