963c69e64f73bcea03a3069395e19313968b2f6e
Hiroki Kondo <kondo.matr02@gmail.com>
2021-10-31 17:08:48 +0900

    ListViewを実装。のちにListView.builderに変更する。

--- creator/lib/screens/home_screen.dart
+++ creator/lib/screens/home_screen.dart
@@ -1,6 +1,14 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:creator/screens/login_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+final List<String> imgList = [
+  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1506355683710-bd071c0a5828?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+];
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key}) : super(key: key);
@@ -9,25 +17,91 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-//参考サイト:https://zuma-lab.com/posts/flutter-nested-list
-
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: Text("ホーム"),
-        iconTheme: IconThemeData(color: Colors.black),
-      ),
-      drawer: _buildDrawer(context),
-      body: ListView.builder(
-          padding: const EdgeInsets.symmetric(
-            vertical: 16,
-          ),
-          itemBuilder: _buildVerticalItem),
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text("ホーム"),
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
+        drawer: _buildDrawer(context),
+        body: ListView(
+          padding: EdgeInsets.all(16),
+          children: [
+            CarouselWithDotsPage(imgList: imgList),
+            // buildQuoteCard(),
+            // buildRoundedCard(),
+            // buildColoredCard(),
+            buildImageCard(),
+            buildImageInteractionCard(),
+            buildImageCard(),
+            buildImageCard(),
+            buildImageCard(),
+            buildImageCard(),
+          ],
+        ));
   }
+
+  Widget buildQuoteCard() => Card(
+        child: Padding(
+          padding: EdgeInsets.all(12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "If life were predictable it would cease to life, and be without flavor.",
+                style: TextStyle(fontSize: 24),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Text(
+                "Eleanor Roosevelt",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      );
+
+  Widget buildRoundedCard() => Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Rounded card",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 4,
+              ),
+              Text(
+                "This card is rounded",
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+        ),
+      );
+
+  Widget buildColoredCard() => Card(
+        shadowColor: Colors.red,
+        elevation: 8,
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+      );
 
   Widget _buildDrawer(BuildContext context) {
     return Drawer(
@@ -50,6 +124,90 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+
+  Widget buildImageCard() => Card(
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Ink.image(
+              image: NetworkImage(
+                'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1327&q=80',
+              ),
+              child: InkWell(
+                onTap: () {},
+              ),
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            Text(
+              "Card With Splash",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 24),
+            ),
+          ],
+        ),
+      );
+
+  Widget buildImageInteractionCard() => Card(
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Ink.image(
+                  image: NetworkImage(
+                    'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1327&q=80',
+                  ),
+                  height: 240,
+                  fit: BoxFit.cover,
+                ),
+                Positioned(
+                  bottom: 16,
+                  right: 16,
+                  left: 16,
+                  child: Text(
+                    "Cats rule the world!",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(16).copyWith(bottom: 0),
+              child: Text(
+                "The cat is the only domesticated species in the family Felidae and is often referred to as the domestic cat to distinguish it from the wild members of the family.",
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+            ButtonBar(
+              alignment: MainAxisAlignment.start,
+              children: [
+                FlatButton(
+                  onPressed: () {},
+                  child: Text('Buy Cat'),
+                ),
+                FlatButton(
+                  onPressed: () {},
+                  child: Text("Buy Cat Food"),
+                )
+              ],
+            ),
+          ],
+        ),
+      );
 
   Widget _buildVerticalItem(BuildContext context, int verticalIndex) {
     return Padding(
@@ -108,59 +266,105 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-// class HomeScreen extends StatefulWidget {
-//   const HomeScreen({Key key}) : super(key: key);
+class CarouselWithDotsPage extends StatefulWidget {
+  List<String> imgList;
+  CarouselWithDotsPage({this.imgList, Key key}) : super(key: key);
 
-//   @override
-//   _HomeScreenState createState() => _HomeScreenState();
-// }
+  @override
+  _CarouselWithDotsPageState createState() => _CarouselWithDotsPageState();
+}
 
-// class _HomeScreenState extends State<HomeScreen> {
-//   @override
+class _CarouselWithDotsPageState extends State<CarouselWithDotsPage> {
+  int _current = 0;
 
-//   //TODO(PR_#9):サロン一覧の画面(概要)を作成する。
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Colors.green,
-//         title: Text('ホーム'),
-//         iconTheme: IconThemeData(color: Colors.black),
-//       ),
-//       drawer: _buildDrawer(context),
-//       body: Center(
-//         child: ElevatedButton(
-//           style: Theme.of(context).elevatedButtonTheme.style.copyWith(
-//                 backgroundColor: MaterialStateProperty.all(Colors.white),
-//                 foregroundColor: MaterialStateProperty.all(Colors.black),
-//               ),
-//           onPressed: () => {
-//             print("ボタンが押された"),
-//           },
-//         ),
-//       ),
-//     );
-//   }
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> imageSliders = widget.imgList
+        .map((item) => Container(
+              child: ClipRRect(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20.0),
+                ),
+                child: Stack(
+                  children: [
+                    Image.network(
+                      item,
+                      fit: BoxFit.cover,
+                      width: 1000,
+                    ),
+                    Positioned(
+                      bottom: 0.0,
+                      left: 0.0,
+                      right: 0.0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color.fromARGB(200, 0, 0, 0),
+                              Color.fromARGB(0, 0, 0, 0),
+                            ],
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                          ),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: 10,
+                        ),
+                        child: Text(
+                          "No. ${widget.imgList.indexOf(item)} image",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ))
+        .toList();
 
-//   Widget _buildDrawer(BuildContext context) {
-//     return Drawer(
-//       child: ListView(
-//         children: [
-//           ListTile(
-//             title: Text("ログアウト"),
-//             onTap: () async {
-//               await FirebaseAuth.instance.signOut();
-//               await Navigator.of(context).pushReplacement(
-//                 MaterialPageRoute(
-//                   builder: (context) {
-//                     return LoginScreen();
-//                   },
-//                 ),
-//               );
-//             },
-//           )
-//         ],
-//       ),
-//     );
-//   }
-// }
-
+    return Column(
+      children: [
+        SizedBox(
+          height: 10,
+        ),
+        CarouselSlider(
+          items: imageSliders,
+          options: CarouselOptions(
+              autoPlay: false,
+              enlargeCenterPage: true,
+              aspectRatio: 2.0,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _current = index;
+                });
+              }),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: widget.imgList.map((url) {
+            int index = widget.imgList.indexOf(url);
+            return Container(
+              width: 8,
+              height: 8,
+              margin: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 3,
+              ),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: _current == index
+                    ? Color.fromRGBO(0, 0, 0, 0.9)
+                    : Color.fromRGBO(0, 0, 0, 0.4),
+              ),
+            );
+          }).toList(),
+        )
+      ],
+    );
+  }
+}
