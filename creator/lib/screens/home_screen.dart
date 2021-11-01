268c6d4c438f5dd26231f63ef0a6ee55a0ef7cc6
Hiroki Kondo <kondo.matr02@gmail.com>
2021-11-01 23:12:59 +0900

    ListView.builderでの実装完了

--- creator/lib/screens/home_screen.dart
+++ creator/lib/screens/home_screen.dart
@@ -1,13 +1,30 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:creator/common/color_filters.dart';
 import 'package:creator/screens/login_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-final List<String> imgList = [
-  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1506355683710-bd071c0a5828?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+final List<String> imgCarouselList = [
+  "https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80",
+  "https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80",
+  "https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80",
+  "https://images.unsplash.com/photo-1506355683710-bd071c0a5828?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+];
+
+final List<String> titleList = [
+  "マタイ6・33",
+  "箴言6・9-11",
+  "イザヤ53・4",
+  "伝道10・17",
+  "箴言1・7"
+];
+
+final List<String> subList = [
+  "空白",
+  "怠け者よ、いつまで寝ているのか。いつ目を覚まして起き上がるのか。少し眠り、少しまどろみ、少し腕を組んで、横になる。すると、付きまとう者のように貧しさが、武装した者のように乏しさがやって来る。",
+  "空白",
+  "幸いなことよ、あなたのような国は。王が貴族の出であり、高官たちが、酔うためではなく力をつけるために、定まった時に食事をする国は。",
+  "空白"
 ];
 
 class HomeScreen extends StatefulWidget {
@@ -21,87 +38,27 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: Text("ホーム"),
-          iconTheme: IconThemeData(color: Colors.black),
-        ),
-        drawer: _buildDrawer(context),
-        body: ListView(
-          padding: EdgeInsets.all(16),
-          children: [
-            CarouselWithDotsPage(imgList: imgList),
-            // buildQuoteCard(),
-            // buildRoundedCard(),
-            // buildColoredCard(),
-            buildImageCard(),
-            buildImageInteractionCard(),
-            buildImageCard(),
-            buildImageCard(),
-            buildImageCard(),
-            buildImageCard(),
-          ],
-        ));
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text("ホーム"),
+        iconTheme: IconThemeData(color: Colors.black),
+      ),
+      drawer: _buildDrawer(context),
+      body: ListView.builder(
+        // ignore: missing_return
+        itemBuilder: (BuildContext context, int index) {
+          if (index == 0) {
+            return CarouselWithDotsPage(imgList: imgCarouselList);
+          } else if (index % 2 == 0) {
+            return buildImageCardOne(index - 1);
+          } else if (index % 2 == 1) {
+            return buildImageCardTwo(index - 1);
+          }
+        },
+        itemCount: titleList.length + 1,
+      ),
+    );
   }
-
-  Widget buildQuoteCard() => Card(
-        child: Padding(
-          padding: EdgeInsets.all(12),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "If life were predictable it would cease to life, and be without flavor.",
-                style: TextStyle(fontSize: 24),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              Text(
-                "Eleanor Roosevelt",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-        ),
-      );
-
-  Widget buildRoundedCard() => Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Container(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Rounded card",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(
-                height: 4,
-              ),
-              Text(
-                "This card is rounded",
-                style: TextStyle(fontSize: 20),
-              ),
-            ],
-          ),
-        ),
-      );
-
-  Widget buildColoredCard() => Card(
-        shadowColor: Colors.red,
-        elevation: 8,
-        clipBehavior: Clip.antiAlias,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(24),
-        ),
-      );
 
   Widget _buildDrawer(BuildContext context) {
     return Drawer(
@@ -125,7 +82,60 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget buildImageCard() => Card(
+  Widget buildImageCardOne(int index) => Card(
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: Stack(
+          alignment: Alignment.topRight,
+          children: [
+            Ink.image(
+              image: NetworkImage(
+                'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1327&q=80',
+              ),
+              colorFilter: ColorFilters.greyscale,
+              child: InkWell(
+                onTap: () {},
+              ),
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            _decolorationOfCardTextMessage(index),
+          ],
+        ),
+      );
+
+  Widget _decolorationOfCardTextMessage(int index) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        SizedBox(height: 24),
+        Center(
+          child: Text(
+            titleList[index],
+            style: TextStyle(
+                fontWeight: FontWeight.bold, color: Colors.white, fontSize: 16),
+          ),
+        ),
+        SizedBox(height: 50),
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text(
+              subList[index],
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 8),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildImageCardTwo(int index) => Card(
         clipBehavior: Clip.antiAlias,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(24),
@@ -144,7 +154,7 @@ class _HomeScreenState extends State<HomeScreen> {
               fit: BoxFit.cover,
             ),
             Text(
-              "Card With Splash",
+              titleList[index],
               style: TextStyle(
                   fontWeight: FontWeight.bold,
                   color: Colors.white,
@@ -153,117 +163,6 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
       );
-
-  Widget buildImageInteractionCard() => Card(
-        clipBehavior: Clip.antiAlias,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(24),
-        ),
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                Ink.image(
-                  image: NetworkImage(
-                    'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1327&q=80',
-                  ),
-                  height: 240,
-                  fit: BoxFit.cover,
-                ),
-                Positioned(
-                  bottom: 16,
-                  right: 16,
-                  left: 16,
-                  child: Text(
-                    "Cats rule the world!",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 24,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.all(16).copyWith(bottom: 0),
-              child: Text(
-                "The cat is the only domesticated species in the family Felidae and is often referred to as the domestic cat to distinguish it from the wild members of the family.",
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-            ButtonBar(
-              alignment: MainAxisAlignment.start,
-              children: [
-                FlatButton(
-                  onPressed: () {},
-                  child: Text('Buy Cat'),
-                ),
-                FlatButton(
-                  onPressed: () {},
-                  child: Text("Buy Cat Food"),
-                )
-              ],
-            ),
-          ],
-        ),
-      );
-
-  Widget _buildVerticalItem(BuildContext context, int verticalIndex) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        vertical: 8,
-        horizontal: 16,
-      ),
-      child: SizedBox(
-        height: 320,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "なんっでやねん",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              "あかんやろ",
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            _buildHorizontalItem(context, verticalIndex)
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildHorizontalItem(BuildContext context, int verticalIndex) {
-    return SizedBox(
-      height: 240,
-      child: PageView.builder(
-        controller: PageController(viewportFraction: 0.8),
-        itemBuilder: (context, horiontalIndex) =>
-            _buildHorizontalView(context, verticalIndex, horiontalIndex),
-      ),
-    );
-  }
-
-  Widget _buildHorizontalView(
-      BuildContext context, int verticalIndex, int horiontalIndex) {
-    final imageUrl =
-        'https://source.unsplash.com/random/275x240?sig=$verticalIndex$horiontalIndex';
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8),
-      child: Card(
-        child: Image.network(imageUrl),
-      ),
-    );
-  }
 }
 
 class CarouselWithDotsPage extends StatefulWidget {
@@ -277,93 +176,90 @@ class CarouselWithDotsPage extends StatefulWidget {
 class _CarouselWithDotsPageState extends State<CarouselWithDotsPage> {
   int _current = 0;
 
+  Widget _bottomTextOfCarousel(String item) {
+    return Positioned(
+      bottom: 0.0,
+      left: 0.0,
+      right: 0.0,
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(200, 0, 0, 0),
+              Color.fromARGB(0, 0, 0, 0),
+            ],
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+          ),
+        ),
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 10,
+        ),
+        child: Text(
+          "No. ${widget.imgList.indexOf(item)} image",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 12.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _dotPageStateOfCarousel() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: widget.imgList.map((url) {
+        int index = widget.imgList.indexOf(url);
+        return Container(
+          width: 8,
+          height: 8,
+          margin: EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 3,
+          ),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: _current == index
+                ? Color.fromRGBO(0, 0, 0, 0.9)
+                : Color.fromRGBO(0, 0, 0, 0.4),
+          ),
+        );
+      }).toList(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final List<Widget> imageSliders = widget.imgList
         .map((item) => Container(
-              child: ClipRRect(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(20.0),
-                ),
+            child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
                 child: Stack(
                   children: [
-                    Image.network(
-                      item,
-                      fit: BoxFit.cover,
-                      width: 1000,
-                    ),
-                    Positioned(
-                      bottom: 0.0,
-                      left: 0.0,
-                      right: 0.0,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [
-                              Color.fromARGB(200, 0, 0, 0),
-                              Color.fromARGB(0, 0, 0, 0),
-                            ],
-                            begin: Alignment.bottomCenter,
-                            end: Alignment.topCenter,
-                          ),
-                        ),
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 20,
-                          vertical: 10,
-                        ),
-                        child: Text(
-                          "No. ${widget.imgList.indexOf(item)} image",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 12.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
+                    Image.network(item, fit: BoxFit.cover, width: 1000),
+                    _bottomTextOfCarousel(item),
                   ],
-                ),
-              ),
-            ))
+                ))))
         .toList();
 
     return Column(
       children: [
-        SizedBox(
-          height: 10,
-        ),
+        SizedBox(height: 10),
         CarouselSlider(
-          items: imageSliders,
-          options: CarouselOptions(
-              autoPlay: false,
-              enlargeCenterPage: true,
-              aspectRatio: 2.0,
-              onPageChanged: (index, reason) {
-                setState(() {
-                  _current = index;
-                });
-              }),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: widget.imgList.map((url) {
-            int index = widget.imgList.indexOf(url);
-            return Container(
-              width: 8,
-              height: 8,
-              margin: EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 3,
-              ),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: _current == index
-                    ? Color.fromRGBO(0, 0, 0, 0.9)
-                    : Color.fromRGBO(0, 0, 0, 0.4),
-              ),
-            );
-          }).toList(),
-        )
+            items: imageSliders,
+            options: CarouselOptions(
+                autoPlay: false,
+                enlargeCenterPage: true,
+                aspectRatio: 2.0,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _current = index;
+                  });
+                })),
+        _dotPageStateOfCarousel()
       ],
     );
   }
